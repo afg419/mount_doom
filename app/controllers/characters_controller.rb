b81c72ec0dba9a88456f5e431ca0565b1009e52e
Aaron Greenspan <afg419@gmail.com>
2016-02-04 10:47:27 -0700

    Prevent not logged in user from visiting character creation page

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,18 @@
 class CharactersController < ApplicationController
+  before_action :user_logged_in?
+
   def new
     @character = Character.new
     @avatars = Avatar.all
     render layout: 'wide',  :locals => {:background => "shire"}
+  end
+
+  private
+
+  def user_logged_in?
+    unless current_user
+      redirect_to login_path
+      flash[:warning] = "Please log in before selecting character"
+    end
   end
 end
