936da1022fb6d787f8fbd6cb966815046daa84ed
Aaron Greenspan <afg419@gmail.com>
2016-02-06 15:45:57 -0700

    Improves test with stubbing prior game

--- test/integration/user_with_prior_game_can_continue_test.rb
+++ test/integration/user_with_prior_game_can_continue_test.rb
@@ -4,20 +4,13 @@ class UserWithPriorGameCanContinueTest < ActionDispatch::IntegrationTest
   test "user can continue prior game" do
     bree = Location.create(name: "Bree", slug: "bree")
     user = create_user
-    login_user
-
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
     skills = SkillSet.create(intelligence: 4, dexterity: 3, strength: 3, health: 3, money: 100, speed: 2)
     avatar = Avatar.create(name: "Taylor", image_url:"jojoj", skill_set: skills)
+    character = Character.create(user: user, avatar: avatar, location: bree)
 
-    visit new_character_path
+    visit user_path(user)
 
-    within ".Taylor" do
-      click_on "Begin Journey!"
-    end
-
-    assert page.has_content?("Help")
-
-    click_on "Save and Quit"
     click_on "Continue"
 
     assert_equal journey_path(bree), current_path
