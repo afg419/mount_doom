80ea794fc669a5d187de1803da4984c493036997
Aaron Greenspan <afg419@gmail.com>
2016-02-09 10:11:13 -0700

    Implements current attribute calculation without equipped items

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,4 @@
 class Character < ActiveRecord::Base
-  extend Forwardable
-  def_delegator :skill_set, :attributes
-
   belongs_to :avatar
   belongs_to :user
   belongs_to :location
@@ -13,19 +10,33 @@ class Character < ActiveRecord::Base
   def current_skills
     avatar_attributes = [avatar.skill_set.attributes]
 
-    weapon_attributes = [equipped_weapon.skill_set.attributes]
-    armor_attributes = [equipped_armor.skill_set.attributes]
 
     apothecary_attributes = items.category_attributes("apothecary")
     inn_item_attributes = items.category_attributes("inn")
 
     character_attributes = avatar_attributes +
-                           weapon_attributes +
-                           armor_attributes +
+                           equipped_weapon_attributes +
+                           equipped_armor_attributes +
                            apothecary_attributes +
                            inn_item_attributes
 
     sum_skills(character_attributes)
+  end
+
+  def equipped_weapon_attributes
+    if equipped_weapon
+      weapon_attributes = [equipped_weapon.skill_set.attributes]
+    else
+      [empty_skills]
+    end
+  end
+
+  def equipped_armor_attributes
+    if equipped_armor
+      armor_attributes = [equipped_armor.skill_set.attributes]
+    else
+      [empty_skills]
+    end
   end
 
   def bank
@@ -48,8 +59,8 @@ class Character < ActiveRecord::Base
 
 private
 
-  def sum_skills(attribute_array)
-    total_skills = {
+  def empty_skills
+    {
       "strength" => 0,
       "dexterity" => 0,
       "intelligence" => 0,
@@ -57,6 +68,10 @@ private
       "health" => 0,
       "money" => 0
     }
+  end
+
+  def sum_skills(attribute_array)
+    total_skills = empty_skills
 
     attribute_array.reduce(total_skills) do |acc, skill_set|
       total_skills.keys.each do |attribute|
