7c064b5ea5addea947bea45f2874b426ad58076b
Taylor Moore <Tmoore2272@gmail.com>
2016-02-10 15:45:29 -0700

    Merges master

--- app/helpers/journey_helper.rb
+++ app/helpers/journey_helper.rb
@@ -6,66 +6,49 @@ module JourneyHelper
         current_character.items = [ create_item("Staff", @blacksmith, "magic", 0, 0, 1, 0, 0, 0, -2),
                                     create_item("Robe", @armory, "armor", 0, 0, 0, 0, 0, 0, -2)
                                   ]
-        current_character.equip_weapon(current_character.items[0])
-        current_character.equip_armor(current_character.items[1])
       when "Aragorn"
         current_character.items = [ create_item("Sword", @blacksmith, "melee", 1, 0, 0, 0, 0, 0, -2),
                                     create_item("Dirty Shirt", @armory, "armor", 0, 0, 0, 0, -1, 0, -2)
                                   ]
-        current_character.equip_weapon(current_character.items[0])
-        current_character.equip_armor(current_character.items[1])
 
       when "Gimli"
         current_character.items = [ create_item("Axe", @blacksmith, "melee", 0, 0, 0, 0, 0, -1, -2),
                                     create_item("Rusty Chainmail", @armory, "armor", 0, 0, 1, 0, 0, 0, -2)
                                   ]
-        current_character.equip_weapon(current_character.items[0])
-        current_character.equip_armor(current_character.items[1])
 
       when "Legolas"
         current_character.items = [ create_item("Bow", @blacksmith, "bow", 0, 0, 0, 0, 0, -1, -2),
                                     create_item("Elvish Shirt", @armory, "armor", 0, 0, 1, 0, 0, 0, -2)
                                   ]
-        current_character.equip_weapon(current_character.items[0])
-        current_character.equip_armor(current_character.items[1])
 
       when "Boromir"
         current_character.items = [ create_item("Sword", @blacksmith, "melee", 1, 0, 0, 0, 0, 0, -2),
                                     create_item("Shirt", @armory, "shirt", 0, 0, 0, 0, -1, 0, -2)
                                   ]
-        current_character.equip_weapon(current_character.items[0])
-        current_character.equip_armor(current_character.items[1])
 
 
       when "Frodo"
         current_character.items = [ create_item("Hobbit Sword", @blacksmith, "melee", 0, 0, 0, 0, 0, 1, -2),
                                     create_item("Shirt", @armory, "armor", 0, 0, 0, 0, -1, 0, -2)
                                   ]
-        current_character.equip_weapon(current_character.items[0])
-        current_character.equip_armor(current_character.items[1])
 
       when "Sam"
         current_character.items = [ create_item("Hobbit Sword", @blacksmith, "melee", 0, 0, 0, 0, 0, 1, -2),
                                     create_item("Shirt", @armory, "armor", 0, 0, 0, 0, -1, 0, -2)
                                   ]
-        current_character.equip_weapon(current_character.items[0])
-        current_character.equip_armor(current_character.items[1])
 
       when "Merry"
         current_character.items = [ create_item("Hobbit Sword", @blacksmith, "melee", 0, 0, 0, 0, 0, 1, -2),
                                     create_item("Shirt", @armory, "armor", 0, 0, 0, 0, -1, 0, -2)
                                   ]
-        current_character.equip_weapon(current_character.items[0])
-        current_character.equip_armor(current_character.items[1])
 
       when "Pippen"
         current_character.items = [ create_item("Hobbit Sword", @blacksmith, "melee", 0, 0, 0, 0, 0, 1, -2),
                                     create_item("Shirt", @armory, "armor", 0, 0, 0, 0, -1, 0, -2)
                                   ]
-        current_character.equip_weapon(current_character.items[0])
-        current_character.equip_armor(current_character.items[1])
-
     end
+    current_character.equip_weapon(current_character.items[0])
+    current_character.equip_armor(current_character.items[1])
   end
 
   def create_item(name, category, label,  strength, defense, intelligence, dexterity, health, speed, money)
