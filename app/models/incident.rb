81525d657a87f16383120ab7a39935191eecbba6
Aaron Greenspan <afg419@gmail.com>
2016-02-10 18:46:40 -0700

    Incidents update current attributes

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,4 +2,8 @@ class Incident < ActiveRecord::Base
   belongs_to :skill_set
   belongs_to :character
   belongs_to :category
+
+  def self.total_injury_attributes
+    all.map{|incident| incident.skill_set.attributes}
+  end
 end
