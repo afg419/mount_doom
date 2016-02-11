d1effd178cd58d2e54c813939a4de26c7ed40504
ShannonPaige <spaige@gmail.com>
2016-02-11 12:14:48 -0700

    update application.rb for heroku

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module SlotaShop
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #added by sp for heroku
+    config.assets.initialize_on_precompile = false
   end
 end
